f1549ace741671090ce508fe2a8ddce3f8c222f3
up201405308 <up201405308@d105935a-5eac-4174-8c7b-96c393d027d5>
2015-10-05 15:41:10 +0000

    timer_display_conf prototype

--- lab2/timer.c
+++ lab2/timer.c
@@ -39,8 +39,57 @@ int timer_get_conf(unsigned long timer, unsigned char *st)
 
 int timer_display_conf(unsigned char conf)
 {
+	//bit 0
+	if ((conf & BIT(0)) == TIMER_BCD)
+		printf("O contador esta em BCD");
+	else
+		printf("O contador esta em binario");
 
-	return 1;
+	//bits 1, 2, e 3
+	if ((conf & BIT(1)) == 0)
+		if ((conf & BIT(2)) == 0)
+			if ((conf & BIT(2)) == 0)
+				printf("Modo 0");
+			else
+				printf("Modo 1");
+		else
+			if ((conf & BIT(2)) == 0)
+				printf("Modo 2");
+			else
+				printf("Modo 3");
+	else
+		if ((conf & BIT(2)) == 0)
+			if ((conf & BIT(2)) == 0)
+				printf("Modo 4");
+			else
+				printf("Modo 5");
+		else
+			if ((conf & BIT(2)) == 0)
+				printf("Modo 2");
+			else
+				printf("Modo 3");
+
+	//bits 4 e 5
+	if ((conf & BIT(4)) == 0 && (conf & BIT(5)) == 1)
+		printf("LSB");
+	else if ((conf & BIT(4)) == 1 && (conf & BIT(5)) == 0)
+		printf("MSB");
+	else if ((conf & BIT(4)) == 1 && (conf & BIT(5)) == 1)
+		printf("LSB seguido de MSB");
+	else
+		return 1;
+
+	//bits 6 e 7
+	if ((conf & BIT(6)) == 0)
+		if ((conf & BIT(7)) == 0)
+			printf("Timer 0");
+		else
+			printf("Timer 1");
+	else if ((conf & BIT(7)) == 0)
+		printf("Timer 2");
+	else
+		return 1;
+	return 0;
 }
 
 int timer_test_square(unsigned long freq) {
