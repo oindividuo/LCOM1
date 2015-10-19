b055096f074ae5255009ad88234ab7e9723beb94
up201405308 <up201405308@d105935a-5eac-4174-8c7b-96c393d027d5>
2015-10-19 14:15:18 +0000

    test_scan driver interrupt loop implemented

--- lab3/i8042.h
+++ lab3/i8042.h
@@ -1,15 +1,35 @@
 #ifndef _LCOM_I8042_H_
 #define _LCOM_I8042_H_
 
+//Registers
+	//status
+#define STAT_REG    0x64
+
+	//input buffer (write)
+#define KBC_CMD_REG 0x64
+#define KBC_ARG_REG 0x60
+#define KBD_CMD_REG 0x60
+#define KBD_ARG_REG 0x60
+
+	//output buffer (read)
+#define KBD_SCNCOD_REG 	0x60
+#define KBC_CMD_RET_REG	0x60
+#define KBD_CMD_RET_REG	0x60
+#define KBD_PRTCL_MSG 	0x60
+
+
+
 //Keys
-#define ESC_MAKE 0x01
-#define ESC_BREAK 0x81
+#define ESC_MAKE	0x01
+#define ESC_BREAK 	0x81
+
 
 
 //Confirmation protocol messages
 #define KBD_ACK		0xFA	//Acknowledgement
 #define KBD_RESEND	0xFB	//Resend
 #define KBD_ERROR	0xFC	//Error
+
 
 
 //Keyboard commands
@@ -19,6 +39,8 @@
 #define KBD_ENABLE  0xF4 	//Clear buffer and enable KBD
 #define KBD_REP     0xF3	//Change KBD repetition rate/delay - bits 0-4 rate, bits 5-6 delay
 #define KBD_LED		0xED	//Switch on/off KBD LEDs - bits 0-2
+
+
 
 //KBC commands
 #define KBC_READ		0x20	//Read Command Byte
--- lab3/test3.c
+++ lab3/test3.c
@@ -1,9 +1,65 @@
+#include "test3.h"
+#include <stdio.h>
+
+static int hook_id;
+
+int kbd_subscribe_int(void ) {
+	hook_id = 1;  //keyboard's IRQ number is 1.
+	if(sys_irqsetpolicy(, IRQ_REENABLE|IRQ_EXCLUSIVE, &hook_id) != OK){ //output the EOI command to the PIC
+		printf("Irqsetpolicy failed \n");
+		return -1;
+	   	   }
+	else if(sys_irqenable((&hook_id)) != OK){ //enables interrupts on the IRQ line associated with the hook_id
+		printf("Irqenable failed \n");
+		return -1;
+		}
+	else return 0;
+}
+
+int kbd_unsubscribe_int() {
+	if ((sys_irqrmpolicy(&hook_id) != OK))
+		return 1;
+		else if (sys_irqdisable(&hook_id) != OK)
+		return 1;
+
+	else return 0;
+}
+
+void kbd_interrupt_handler(){ //  reads the bytes from the KBC’s OUT_BUF
+
+}
+
 int kbd_test_scan(unsigned short ass) {
-	/* To be completed */
+
+	 int ipc_status, r, irq_set = 0;
+		 message msg;
+	     irq_set = timer_subscribe_int();
+	     if(irq_set != 0)
+	    	 return 1;
+	     irq_set = BIT(irq_set);
+	while(!= ESC_BREAK)) {  //Interrupt loop
+	             	     /* Get a request message. */
+	             		r = driver_receive(ANY, &msg, &ipc_status);
+	             	    if ( r != 0 ) {
+	             	      printf("driver_receive failed with: %d", r);
+	             	        continue;
+	             	    }
+	             	    if (is_ipc_notify(ipc_status)) { /* received notification */
+	             	       switch (_ENDPOINT_P(msg.m_source)) {
+	             	           case HARDWARE: /* hardware interrupt notification */
+	             	              if (msg.NOTIFY_ARG & irq_set) {
+
+	             	              }
+	             	               break;
+	             	         default:
+	             	        break; /* no other notifications expected: do nothing */
+	             	   }
+	             	} else { /* received a standard message, not a notification */
+	             	     /* no standard messages expected: do nothing */
+	             	       }
+	             	}
+
+	if(kdb_unsubscribe_int()!= 0) //in order to use Minix 3 virtual terminals
+		return 1;
 }
-int kbd_test_leds(unsigned short n, unsigned short *leds) {
-    /* To be completed */
-}
-int kbd_test_timed_scan(unsigned short n) {
-    /* To be completed */
-}
+Chat conversation end
