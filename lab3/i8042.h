447c9023ef1d134d6ace32e0f9eaf3f982c1c225
up201405308 <up201405308@d105935a-5eac-4174-8c7b-96c393d027d5>
2015-10-19 14:00:27 +0000

    updated i8042.h

--- lab3/i8042.h
+++ lab3/i8042.h
@@ -5,11 +5,30 @@
 #define ESC_MAKE 0x01
 #define ESC_BREAK 0x81
 
-//Commands
-#define KBD_RESET 0xFF   //Reset KBD
-#define KBD_DEFAULT 0xF6 //Set default values and enable KBD
-#define KBD_DISABLE 0xF5 //Disable KBD (set default values and stop scanning)
-#define KBD_ENABLE  0xF4 //Clear buffer and enable KBD
+
+//Confirmation protocol messages
+#define KBD_ACK		0xFA	//Acknowledgement
+#define KBD_RESEND	0xFB	//Resend
+#define KBD_ERROR	0xFC	//Error
+
+
+//Keyboard commands
+#define KBD_RESET   0xFF 	//Reset KBD
+#define KBD_DEFAULT 0xF6 	//Set default values and enable KBD
+#define KBD_DISABLE 0xF5 	//Disable KBD (set default values and stop scanning)
+#define KBD_ENABLE  0xF4 	//Clear buffer and enable KBD
+#define KBD_REP     0xF3	//Change KBD repetition rate/delay - bits 0-4 rate, bits 5-6 delay
+#define KBD_LED		0xED	//Switch on/off KBD LEDs - bits 0-2
+
+//KBC commands
+#define KBC_READ		0x20	//Read Command Byte
+#define KBC_WRITE		0x60	//Write Command Byte
+#define KBC_CHECK		0xAA	//Check KBC (Self-test)
+#define KBC_CHECK_ITF	0xAB	//Check Keyboard Interface
+#define KBC_ENABLE		0xAE	//Enable KBD Interface
+#define KBC_DISABLE		0xAD	//Disable KBD Interface
+
+
 
 
 
